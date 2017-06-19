86632006e10033ac37803bcc95128ab9236447ea
york yao <yaoao12306@outlook.com>
2017-06-19 19:10:42 +0800

    better import

--- demo/vue/index.ts
+++ demo/vue/index.ts
@@ -1,4 +1,4 @@
-import * as Vue from "vue";
+import Vue from "vue";
 import Component from "vue-class-component";
 import "../../dist/vue";
 import { data, clearSelectionOfTree, toggle, setSelectionOfTree, setParentsSelection, move, canMove, setContextMenu } from "../common";
