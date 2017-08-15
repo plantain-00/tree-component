fad4efe01ab15dd39311f3741f18645b04d70483
york yao <yaoao12306@outlook.com>
2017-08-15 19:25:38 +0800

    improve types

--- src/common.ts
+++ src/common.ts
@@ -35,9 +35,9 @@ export type ContextMenuData<T = any> = {
 };
 
 import { __extends, __decorate, __assign } from "tslib";
-(window as any).__extends = __extends;
-(window as any).__decorate = __decorate;
-(window as any).__assign = __assign;
+window.__extends = __extends;
+window.__decorate = __decorate;
+window.__assign = __assign;
 
 export class DoubleClick {
     clicked = false;
---
+++ src/lib.d.ts
@@ -0,0 +1,5 @@
+declare interface Window {
+    __extends: any;
+    __decorate: any;
+    __assign: any;
+}
