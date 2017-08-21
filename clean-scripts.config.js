c55edc04ac1e0c3b73abeb403554df94cef3f6de
york yao <yaoao12306@outlook.com>
2017-08-21 09:56:15 +0800

    improve script

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -18,9 +18,9 @@ module.exports = {
         `image2base64-cli images/*.png images/*.gif --less src/variables.less --base images`,
         [
           `lessc src/tree.less > dist/tree.css`,
-          `cleancss -o dist/tree.min.css dist/tree.css`
-        ],
-        `cleancss -o demo/index.bundle.css dist/tree.min.css ./node_modules/github-fork-ribbon-css/gh-fork-ribbon.css`
+          `cleancss -o dist/tree.min.css dist/tree.css`,
+          `cleancss -o demo/index.bundle.css dist/tree.min.css ./node_modules/github-fork-ribbon-css/gh-fork-ribbon.css`
+        ]
       ],
       clean: `rimraf demo/**/index.bundle-*.js demo/tree-icon-*.png demo/index.bundle-*.css`
     },
@@ -60,7 +60,7 @@ module.exports = {
     angular: `file2variable-cli src/vue-node.template.html src/vue-tree.template.html -o src/vue-variables.ts --html-minify --base src --watch`,
     src: `tsc -p src --watch`,
     demo: `tsc -p demo --watch`,
-    webpack: `webpack --display-modules --config demo/webpack.config.js --watch`,
+    webpack: `webpack --config demo/webpack.config.js --watch`,
     image: `image2base64-cli images/*.png images/*.gif --less src/variables.less --base images --watch`,
     less: `watch-then-execute "src/*.less" --script "clean-scripts build[2].css[1]"`,
     rev: `rev-static --config demo/rev-static.config.js --watch`
