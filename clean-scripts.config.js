7d96d94dddae53bcbdec7cc27226b6f16c5a342a
york yao <yaoao12306@outlook.com>
2017-08-21 09:28:42 +0800

    improve watch script

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -16,8 +16,10 @@ module.exports = {
       ],
       css: [
         `image2base64-cli images/*.png images/*.gif --less src/variables.less --base images`,
-        `lessc src/tree.less > dist/tree.css`,
-        `cleancss -o dist/tree.min.css dist/tree.css`,
+        [
+          `lessc src/tree.less > dist/tree.css`,
+          `cleancss -o dist/tree.min.css dist/tree.css`
+        ],
         `cleancss -o demo/index.bundle.css dist/tree.min.css ./node_modules/github-fork-ribbon-css/gh-fork-ribbon.css`
       ],
       clean: `rimraf demo/**/index.bundle-*.js demo/tree-icon-*.png demo/index.bundle-*.css`
@@ -53,5 +55,14 @@ module.exports = {
     less: `stylelint --fix "src/**/*.less"`
   },
   release: `clean-release`,
-  watch: `watch-then-execute "src/**/*.ts" "src/**/*.tsx" "demo/**/*.ts" "demo/**/*.tsx" "src/**/*.template.html" --exclude "src/compiled/**/*,src/*-variables.ts" --script "npm run build"`
+  watch: {
+    vue: `file2variable-cli src/angular-node.template.html src/angular-tree.template.html -o src/angular-variables.ts --html-minify --base src --watch`,
+    angular: `file2variable-cli src/vue-node.template.html src/vue-tree.template.html -o src/vue-variables.ts --html-minify --base src --watch`,
+    src: `tsc -p src --watch`,
+    demo: `tsc -p demo --watch`,
+    webpack: `webpack --display-modules --config demo/webpack.config.js --watch`,
+    image: `image2base64-cli images/*.png images/*.gif --less src/variables.less --base images --watch`,
+    less: `watch-then-execute "src/*.less" --script "clean-scripts build[2].css[1]"`,
+    rev: `rev-static --config demo/rev-static.config.js --watch`
+  }
 }
