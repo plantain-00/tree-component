212d546a06a91b2c8eddf556cde3818070dae17d
york yao <yaoao12306@outlook.com>
2018-03-04 17:30:53 +0800

    chore: update dependencies

--- packages/angular/demo/jit/webpack.config.js
+++ packages/angular/demo/jit/webpack.config.js
@@ -5,20 +5,5 @@ module.exports = {
   output: {
     path: __dirname,
     filename: 'index.bundle.js'
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    }),
-    new webpack.NoEmitOnErrorsPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      output: {
-        comments: false
-      },
-      exclude: [
-      ]
-    })
-  ]
+  }
 }
--- packages/vue/demo/webpack.config.js
+++ packages/vue/demo/webpack.config.js
@@ -6,21 +6,6 @@ module.exports = {
     path: __dirname,
     filename: 'index.bundle.js'
   },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    }),
-    new webpack.NoEmitOnErrorsPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      output: {
-        comments: false
-      },
-      exclude: [
-      ]
-    })
-  ],
   resolve: {
     alias: {
       'vue$': 'vue/dist/vue.esm.js'
