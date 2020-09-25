efa8a6114d615aedf8c8216c6d6264aac9442a2a
York Yao <me@yorkyao.com>
2020-09-25 21:07:12 +0800

    feat: support vue3

--- clean-scripts.config.ts
+++ clean-scripts.config.ts
@@ -58,7 +58,7 @@ export default {
     markdown: `markdownlint README.md`,
     typeCoverage: {
       core: 'cd packages/core && type-coverage -p src --strict',
-      vue: 'cd packages/vue && type-coverage -p src --strict',
+      vue: 'cd packages/vue && type-coverage -p src --strict --ignore-files "src/variables.ts"',
       react: 'cd packages/react && type-coverage -p src --strict'
     }
   },
--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -1,280 +1,289 @@
-import Vue from 'vue'
-import Component from 'vue-class-component'
+import { defineComponent, PropType } from 'vue'
 import * as common from 'tree-component'
 export * from 'tree-component'
-import { nodeTemplateHtml, nodeTemplateHtmlStatic, treeTemplateHtml, treeTemplateHtmlStatic } from './variables'
+import { nodeTemplateHtml, treeTemplateHtml } from './variables'
 
-@Component({
-  render: nodeTemplateHtml,
-  staticRenderFns: nodeTemplateHtmlStatic,
-  props: ['data', 'last', 'checkbox', 'path', 'draggable', 'root', 'zindex', 'preid']
-})
-export class Node<T> extends Vue {
-  data!: common.TreeData<T>
-  last!: boolean
-  checkbox?: boolean
-  path!: number[]
-  draggable?: boolean
-  root!: common.TreeData<T>[]
-  zindex?: number
-  preid?: string
-
-  contextmenuVisible = false
-  get contextmenuStyle() {
+/**
+ * @public
+ */
+export const Node = defineComponent({
+  props: {
+    data: {
+      type: Object as PropType<common.TreeData<unknown>>,
+      required: true,
+    },
+    last: {
+      type: Boolean,
+      required: true,
+    },
+    checkbox: Boolean,
+    path: {
+      type: Array as PropType<number[]>,
+      required: true,
+    },
+    draggable: {
+      type: Boolean,
+      required: true,
+    },
+    root: Array as PropType<common.TreeData<unknown>[]>,
+    zindex: Number,
+    preid: {
+      type: String,
+      required: true,
+    },
+  },
+  data: () => {
     return {
-      'position': 'absolute',
-      'left': '0px',
-      'top': '0px',
-      'z-index': typeof this.zindex === 'number' ? this.zindex : 1
+      contextmenuVisible: false,
+      hovered: false,
+      doubleClick: new common.DoubleClick(),
     }
-  }
-  private hovered = false
-  private doubleClick = new common.DoubleClick()
-
-  get nodeClassName() {
-    return common.getNodeClassName(this.data, this.last, !!this.$slots.default)
-  }
-
-  get anchorClassName() {
-    return common.getAnchorClassName(this.data, this.hovered, this.path)
-  }
-
-  get checkboxClassName() {
-    return common.getCheckboxClassName(this.data, this.path)
-  }
-
-  get iconClassName() {
-    return common.getIconClassName(this.data.icon)
-  }
-
-  get oclClassName() {
-    return common.getOclClassName(this.path)
-  }
-
-  get pathString() {
-    return this.path.toString()
-  }
-
-  get hasMarker() {
-    return this.draggable && this.data.state.dropPosition !== common.DropPosition.empty
-  }
-
-  get markerClassName() {
-    return common.getMarkerClassName(this.data)
-  }
-
-  private get eventData(): common.EventData<T> {
-    return {
-      data: this.data,
-      path: this.path
-    }
-  }
-  get contextmenuData(): common.ContextMenuData<T> {
-    return {
-      data: this.data,
-      path: this.path,
-      root: this.root
-    }
-  }
-  get id() {
-    return common.getId(this.path, this.preid)
-  }
-
-  geChildPath(index: number) {
-    return this.path.concat(index)
-  }
-
-  hover(hovered: boolean) {
-    this.hovered = hovered
-    if (!hovered) {
-      this.contextmenuVisible = false
-    }
-  }
-  ontoggle(eventData?: common.EventData<T>) {
-    if (eventData) {
-      this.$emit('toggle', eventData)
-    } else {
-      if (this.data.state.openable || this.data.children.length > 0 || this.$slots.default) {
-        this.$emit('toggle', this.eventData)
+  },
+  computed: {
+    contextmenuStyle(): { [key: string]: unknown } {
+      return {
+        'position': 'absolute',
+        'left': '0px',
+        'top': '0px',
+        'z-index': typeof this.zindex === 'number' ? this.zindex : 1
       }
+    },
+    nodeClassName(): string {
+      return common.getNodeClassName(this.data, this.last, !!this.$slots.default)
+    },
+    anchorClassName(): string {
+      return common.getAnchorClassName(this.data, this.hovered, this.path)
+    },
+    checkboxClassName(): string {
+      return common.getCheckboxClassName(this.data, this.path)
+    },
+    iconClassName(): string {
+      return common.getIconClassName(this.data.icon)
+    },
+    oclClassName(): string {
+      return common.getOclClassName(this.path)
+    },
+    pathString(): string {
+      return this.path.toString()
+    },
+    hasMarker(): boolean {
+      return this.draggable && this.data.state.dropPosition !== common.DropPosition.empty
+    },
+    markerClassName(): string {
+      return common.getMarkerClassName(this.data)
+    },
+    eventData(): common.EventData<unknown> {
+      return {
+        data: this.data,
+        path: this.path
+      }
+    },
+    id(): string| undefined {
+      return common.getId(this.path, this.preid)
     }
-  }
-  onchange(eventData?: common.EventData<T>) {
-    if (eventData) {
+  },
+  methods: {
+    geChildPath(index: number) {
+      return this.path.concat(index)
+    },
+    hover(hovered: boolean) {
+      this.hovered = hovered
+      if (!hovered) {
+        this.contextmenuVisible = false
+      }
+    },
+    ontoggle(eventData?: common.EventData<unknown>) {
+      if (eventData) {
+        this.$emit('toggle', eventData)
+      } else {
+        if (this.data.state.openable || this.data.children.length > 0 || this.$slots.default) {
+          this.$emit('toggle', this.eventData)
+        }
+      }
+    },
+    onchange(eventData?: common.EventData<unknown>) {
+      if (eventData) {
+        this.$emit('change', eventData)
+      } else {
+        if (this.data.state.disabled) {
+          return
+        }
+
+        this.doubleClick.onclick(() => {
+          this.$emit('change', this.eventData)
+        })
+      }
+    },
+    oncontextmenu(e: MouseEvent) {
+      this.contextmenuVisible = true
+      this.contextmenuStyle.left = e.offsetX + 'px'
+      this.contextmenuStyle.top = e.offsetY + 'px'
+      e.preventDefault()
+      return false
+    },
+    ondragstart(event: DragEvent) {
+      this.$emit('dragstart', event)
+    },
+    ondragend(event: DragEvent) {
+      this.$emit('dragend', event)
+    },
+    ondragover(event: DragEvent) {
+      this.$emit('dragover', event)
+    },
+    ondragenter(event: DragEvent) {
+      this.$emit('dragenter', event)
+    },
+    ondragleave(event: DragEvent) {
+      this.$emit('dragleave', event)
+    },
+    ondrop(event: DragEvent) {
+      this.$emit('drop', event)
+    },
+  },
+  render: nodeTemplateHtml,
+})
+
+/**
+ * @public
+ */
+export const Tree = defineComponent({
+  render: treeTemplateHtml,
+  props: {
+    data: {
+      type: Array as PropType<common.TreeData<unknown>[]>,
+      required: true,
+    },
+    checkbox: Boolean,
+    draggable: Boolean,
+    nodots: Boolean,
+    size: String,
+    theme: String,
+    dropAllowed: Function as PropType<(dropData: common.DropData<unknown>) => boolean>,
+    zindex: Number,
+    preid: String,
+    dragTarget: Object as PropType<common.DragTargetData<unknown> | null>,
+  },
+  data: () => {
+    return {
+      localDragTarget: null as HTMLElement | null,
+      dropTarget: null as HTMLElement | null,
+    }
+  },
+  computed: {
+    rootClassName(): string {
+      return common.getRootClassName(this.checkbox, this.size, this.theme)
+    },
+    containerClassName(): string {
+      return common.getContainerClassName(this.nodots)
+    }
+  },
+  methods: {
+    ontoggle(eventData: common.EventData<unknown>) {
+      this.$emit('toggle', eventData)
+    },
+    onchange(eventData: common.EventData<unknown>) {
       this.$emit('change', eventData)
-    } else {
-      if (this.data.state.disabled) {
+    },
+    ondragstart(event: DragEvent) {
+      if (!this.draggable) {
         return
       }
-
-      this.doubleClick.onclick(() => {
-        this.$emit('change', this.eventData)
+      this.localDragTarget = event.target as HTMLElement
+      this.dropTarget = event.target as HTMLElement
+      this.$emit('change-drag-target', {
+        target: event.target as HTMLElement,
+        root: this.data
       })
-    }
-  }
-  oncontextmenu(e: MouseEvent) {
-    this.contextmenuVisible = true
-    this.contextmenuStyle.left = e.offsetX + 'px'
-    this.contextmenuStyle.top = e.offsetY + 'px'
-    e.preventDefault()
-    return false
-  }
-  ondragstart(event: DragEvent) {
-    this.$emit('dragstart', event)
-  }
-  ondragend(event: DragEvent) {
-    this.$emit('dragend', event)
-  }
-  ondragover(event: DragEvent) {
-    this.$emit('dragover', event)
-  }
-  ondragenter(event: DragEvent) {
-    this.$emit('dragenter', event)
-  }
-  ondragleave(event: DragEvent) {
-    this.$emit('dragleave', event)
-  }
-  ondrop(event: DragEvent) {
-    this.$emit('drop', event)
-  }
-}
-
-Vue.component('node', Node)
-
-@Component({
-  render: treeTemplateHtml,
-  staticRenderFns: treeTemplateHtmlStatic,
-  props: ['data', 'checkbox', 'draggable', 'nodots', 'size', 'theme', 'dropAllowed', 'dragTarget', 'zindex', 'preid']
-})
-export class Tree<T> extends Vue {
-  data!: common.TreeData<T>[]
-  checkbox?: boolean
-  draggable?: boolean
-  nodots?: boolean
-  size?: string
-  theme?: string
-  dropAllowed?: (dropData: common.DropData<T>) => boolean
-  zindex?: number
-  preid?: string
-  dragTarget?: common.DragTargetData | null
-
-  private localDragTarget: HTMLElement | null = null
-  private dropTarget: HTMLElement | null = null
-
-  get rootClassName() {
-    return common.getRootClassName(this.checkbox, this.size, this.theme)
-  }
-  get containerClassName() {
-    return common.getContainerClassName(this.nodots)
-  }
-
-  ontoggle(eventData: common.EventData<T>) {
-    this.$emit('toggle', eventData)
-  }
-  onchange(eventData: common.EventData<T>) {
-    this.$emit('change', eventData)
-  }
-  ondragstart(event: DragEvent) {
-    if (!this.draggable) {
-      return
-    }
-    this.localDragTarget = event.target as HTMLElement
-    this.dropTarget = event.target as HTMLElement
-    this.$emit('change-drag-target', {
-      target: event.target as HTMLElement,
-      root: this.data
-    })
-  }
-  ondragend(event: DragEvent) {
-    if (!this.draggable) {
-      return
-    }
-    this.localDragTarget = null
-    for (const tree of this.data) {
-      common.clearMarkerOfTree(tree)
-    }
-    this.$emit('change-drag-target', null)
-  }
-  ondragover(event: DragEvent) {
-    if (!this.canDrop(event)) {
-      return
-    }
-    if (this.dragTarget) {
-      common.ondrag(event.pageY, this.dragTarget.target, this.dropTarget, this.dragTarget.root, this.data, this.dropAllowed)
-    } else {
-      common.ondrag(event.pageY, this.localDragTarget, this.dropTarget, this.data, this.data, this.dropAllowed)
-    }
-    event.preventDefault()
-  }
-  ondragenter(event: DragEvent) {
-    if (!this.canDrop(event)) {
-      return
-    }
-    this.dropTarget = this.getAnchor(event.target as HTMLElement)
-    if (this.dragTarget) {
-      common.ondrag(event.pageY, this.dragTarget.target, this.dropTarget, this.dragTarget.root, this.data, this.dropAllowed)
-    } else {
-      common.ondrag(event.pageY, this.localDragTarget, this.dropTarget, this.data, this.data, this.dropAllowed)
-    }
-  }
-  ondragleave(event: DragEvent) {
-    if (!this.canDrop(event)) {
-      return
-    }
-    const target = this.getAnchor(event.target as HTMLElement)
-    const containsTarget = this.containsNode(this.dropTarget as HTMLElement, event.target as HTMLElement)
-    if (!containsTarget && target === this.dropTarget) {
-      this.dropTarget = null
-    }
-    common.ondragleave(target as HTMLElement, this.data)
-  }
-  ondrop(event: DragEvent) {
-    if (!this.canDrop(event)) {
-      return
-    }
-    if (this.dragTarget) {
-      common.ondrop(this.getAnchor(event.target as HTMLElement), this.dragTarget.target, this.dragTarget.root, this.data, dropData => {
-        this.$emit('drop', dropData)
-      })
-    } else {
-      common.ondrop(this.getAnchor(event.target as HTMLElement), this.localDragTarget, this.data, this.data, dropData => {
-        this.$emit('drop', dropData)
-      })
-    }
-  }
-  private canDrop(event: DragEvent) {
-    if (!this.draggable || !event.target) {
-      return false
-    }
-    const hasPath = (event.target as HTMLElement).dataset && (event.target as HTMLElement).dataset.path
-    if (!hasPath && (event.target as HTMLElement).classList && !(event.target as HTMLElement).classList.contains('tree-node')) {
+    },
+    ondragend(event: DragEvent) {
+      if (!this.draggable) {
+        return
+      }
+      this.localDragTarget = null
+      for (const tree of this.data) {
+        common.clearMarkerOfTree(tree)
+      }
+      this.$emit('change-drag-target', null)
+    },
+    ondragover(event: DragEvent) {
+      if (!this.canDrop(event)) {
+        return
+      }
+      if (this.dragTarget) {
+        common.ondrag(event.pageY, this.dragTarget.target, this.dropTarget, this.dragTarget.root, this.data, this.dropAllowed)
+      } else {
+        common.ondrag(event.pageY, this.localDragTarget, this.dropTarget, this.data, this.data, this.dropAllowed)
+      }
+      event.preventDefault()
+    },
+    ondragenter(event: DragEvent) {
+      if (!this.canDrop(event)) {
+        return
+      }
+      this.dropTarget = this.getAnchor(event.target as HTMLElement)
+      if (this.dragTarget) {
+        common.ondrag(event.pageY, this.dragTarget.target, this.dropTarget, this.dragTarget.root, this.data, this.dropAllowed)
+      } else {
+        common.ondrag(event.pageY, this.localDragTarget, this.dropTarget, this.data, this.data, this.dropAllowed)
+      }
+    },
+    ondragleave(event: DragEvent) {
+      if (!this.canDrop(event)) {
+        return
+      }
       const target = this.getAnchor(event.target as HTMLElement)
-      return target.dataset && target.dataset.path
-    }
-    return hasPath
-  }
-  private containsNode(parentNode: HTMLElement, node: HTMLElement): boolean {
-    for (let i = 0; i < parentNode.children.length; i++) {
-      const child = parentNode.children[i] as HTMLElement
-      if (child === node) {
-        return true
+      const containsTarget = this.containsNode(this.dropTarget as HTMLElement, event.target as HTMLElement)
+      if (!containsTarget && target === this.dropTarget) {
+        this.dropTarget = null
       }
-      if (child.children) {
-        return this.containsNode(child, node)
+      common.ondragleave(target as HTMLElement, this.data)
+    },
+    ondrop(event: DragEvent) {
+      if (!this.canDrop(event)) {
+        return
       }
+      if (this.dragTarget) {
+        common.ondrop(this.getAnchor(event.target as HTMLElement), this.dragTarget.target, this.dragTarget.root, this.data, dropData => {
+          this.$emit('drop', dropData)
+        })
+      } else {
+        common.ondrop(this.getAnchor(event.target as HTMLElement), this.localDragTarget, this.data, this.data, dropData => {
+          this.$emit('drop', dropData)
+        })
+      }
+    },
+    canDrop(event: DragEvent) {
+      if (!this.draggable || !event.target) {
+        return false
+      }
+      const hasPath = (event.target as HTMLElement).dataset && (event.target as HTMLElement).dataset.path
+      if (!hasPath && (event.target as HTMLElement).classList && !(event.target as HTMLElement).classList.contains('tree-node')) {
+        const target = this.getAnchor(event.target as HTMLElement)
+        return target.dataset && target.dataset.path
+      }
+      return hasPath
+    },
+    containsNode(parentNode: HTMLElement, node: HTMLElement): boolean {
+      for (let i = 0; i < parentNode.children.length; i++) {
+        const child = parentNode.children[i] as HTMLElement
+        if (child === node) {
+          return true
+        }
+        if (child.children) {
+          return this.containsNode(child, node)
+        }
+      }
+      return false
+    },
+    getAnchor(target: HTMLElement) {
+      let anchor = target
+      while (anchor && anchor.classList && !anchor.classList.contains('tree-anchor')) {
+        anchor = anchor.parentElement as HTMLElement
+      }
+      if (anchor && anchor.classList && anchor.classList.contains('tree-anchor')) {
+        return anchor
+      }
+      return target
     }
-    return false
   }
-  private getAnchor(target: HTMLElement) {
-    let anchor = target
-    while (anchor && anchor.classList && !anchor.classList.contains('tree-anchor')) {
-      anchor = anchor.parentElement as HTMLElement
-    }
-    if (anchor && anchor.classList && anchor.classList.contains('tree-anchor')) {
-      return anchor
-    }
-    return target
-  }
-}
-
-Vue.component('tree', Tree)
+})
